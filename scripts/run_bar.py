7ebbc6019021a507f60d9b7bba836e13fdef735d
Ana Bonaca <abonaca@gmail.com>
2022-06-10 01:07:22 -0700

    add test option

--- scripts/run_bar.py
+++ scripts/run_bar.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
     """Wrapper to run bar models"""
     
     args = sys.argv
+    if len(args)<5:
+        args += [False]
     
-    evolve_bar_stars(mw_label=args[1], Nskip=int(args[2]), iskip=int(args[3]))
+    evolve_bar_stars(mw_label=args[1], Nskip=int(args[2]), iskip=int(args[3]), test=bool(args[4]))
