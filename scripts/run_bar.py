47d2689a118353db669b146dacdc731de77032e0
Ana Bonaca <abonaca@gmail.com>
2022-06-27 11:26:50 -0700

    run idealized disk and bar

--- scripts/run_bar.py
+++ scripts/run_bar.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     if len(args)<5:
         args += [False]
     
-    evolve_bar_stars(mw_label=args[1], Nskip=int(args[2]), iskip=int(args[3]), test=bool(args[4]), m=5e9*u.Msun)
+    evolve_bar_stars(mw_label=args[1], Nskip=int(args[2]), iskip=int(args[3]), test=bool(args[4]), m=1e10*u.Msun, Nrand=4000)
